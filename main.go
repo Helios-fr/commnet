fc85e8c8a9892ee95844de714a4356a3d7ab5ee3
Nyx <79072303+Helios-fr@users.noreply.github.com>
2024-09-08 18:08:37 +1000

    [tests] Ported tests to new main.go

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+
+	"commnet/backend/encryption"
+	"commnet/backend/users"
 )
 
 //go:embed all:frontend/build
 var assets embed.FS
 
 func main() {
+	// Run the tests on the backend
+	tests()
+
 	// Create an instance of the app structure
 	app := NewApp()
 
@@ -32,5 +39,24 @@ func main() {
 
 	if err != nil {
 		println("Error:", err.Error())
+	}
+}
+
+func tests() {
+	if !encryption.Test_All() {
+		log.Println("Encryption tests failed")
+		log.Println("Generated keys:", encryption.Test_GenerateKeyPair())
+		log.Println("Encrypted message:", encryption.Test_Encrypt())
+		log.Println("Decrypted message:", encryption.Test_Decrypt())
+	}
+	if !users.Test_All() {
+		log.Println("Users tests failed")
+		log.Println("Create user:", users.Test_CreateUser())
+		log.Println("Get user:", users.Test_GetUser())
+		log.Println("Update user:", users.Test_UpdateUser())
+		log.Println("Validate user:", users.Test_ValidateUser())
+		log.Println("Remove user:", users.Test_RemoveUser())
+		log.Println("Reset DB:", users.Test_ResetDB())
+		log.Println("Get Authority:", users.Test_GetAuthority())
 	}
 }
